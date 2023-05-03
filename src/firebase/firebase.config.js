511facb4e4502189e29d6da0ba88cc0ca02a5091
sakibbiswas <sakibsakib99880@gmail.com>
2023-05-03 14:55:43 +0600

    Environment variable add

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0851r4PakWTWQzJo6HzDrOln0jJzpyEI",
-  authDomain: "chef-recipe-client-8ef77.firebaseapp.com",
-  projectId: "chef-recipe-client-8ef77",
-  storageBucket: "chef-recipe-client-8ef77.appspot.com",
-  messagingSenderId: "948588615347",
-  appId: "1:948588615347:web:7291cd0e1f565dbc0902c6"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain ,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
